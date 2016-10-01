1b0b1b2f548b76e65e49573d945d2d08d6c7701d
HueyPark <jaewan.huey.park@gmail.com>
2016-10-02 00:30:51 +0900

    Add SnakeBody

--- Snake/Content/Scripts/snake_body.py
+++ Snake/Content/Scripts/snake_body.py
@@ -1,15 +1,10 @@
 from config import SNAKE_MOVE_SPEED
 
 
-class SnakeHead:
+class SnakeBody:
     def tick(self, delta_time):
         location = self.uobject.get_actor_location()
 
         location += self.uobject.get_actor_forward() * SNAKE_MOVE_SPEED * delta_time
 
         self.uobject.set_actor_location(location)
-
-    def __spawn_body(self):
-        snake_body = self.uobject.call_function('SpawnSnakeBody')
-
-
