578819a6d83412d3ab580ab061e2c26cdfce306e
HueyPark <jaewan.huey.park@gmail.com>
2016-10-08 14:07:39 +0900

    Add SnakeHead turn

--- Snake/Content/Scripts/snake_head.py
+++ Snake/Content/Scripts/snake_head.py
@@ -2,12 +2,23 @@ from config import SNAKE_MOVE_SPEED
 
 
 class SnakeHead:
+    def begin_play(self):
+        self.uobject.bind_axis('TurnRate', self.__turn)
+
     def tick(self, delta_time):
+        self.__move_forward(delta_time)
+
+    def __move_forward(self, delta_time):
         location = self.uobject.get_actor_location()
-
         location += self.uobject.get_actor_forward() * SNAKE_MOVE_SPEED * delta_time
-
         self.uobject.set_actor_location(location)
+
+    def __turn(self, axis_value):
+        turn_rate = axis_value * self.uobject.get_world_delta_seconds() * 45
+
+        rotation = self.uobject.get_actor_rotation()
+        rotation.yaw += turn_rate
+        self.uobject.set_actor_rotation(rotation)
 
     def __spawn_body(self):
         snake_body = self.uobject.call_function('SpawnSnakeBody')
