b7560fceedcfb5a0b2e83619b28578197a6396bd
HueyPark <jaewan.huey.park@gmail.com>
2016-10-18 07:17:45 +0900

    Delete: UnrealEnginePython plugin

--- Snake/Plugins/UnrealEnginePython/Source/UnrealEnginePython/Private/UEPyPhysics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-
-#include "UnrealEnginePython.h"
-
-PyObject *py_ue_set_simulate_physics(ue_PyUObject *, PyObject *);
-PyObject *py_ue_destructible_apply_damage(ue_PyUObject *, PyObject *);
